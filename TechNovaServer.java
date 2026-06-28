8d51db54f5bcdc5844ab323805bf12450c5901ba
navanithiselvam01-art <navanithiselvam01@gmail.com>
2026-06-29 02:52:18 +0530

    Change server port from 8080 to 8081

--- TechNovaServer.java
+++ TechNovaServer.java
@@ -9,7 +9,7 @@ public class TechNovaServer {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
 
         server.createContext("/", exchange -> {
 
